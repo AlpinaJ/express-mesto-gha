fc80d1b1f90b5e449a9284c770c5b66f0bc4d308
julia953197 <julia953197@yandex.ru>
2022-04-26 16:12:22 +0300

    Fixed github tests: finding user

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -5,58 +5,67 @@ const ErrorNotFoundCode = 404;
 const ValidationErrorCode = 400;
 
 module.exports.getUsers = (req, res) => {
-  User.find({ })
-    .then((users) => res.send({ data: users }));
+  User.find({})
+    .then((users) => res.send({data: users}));
 };
 
 module.exports.getUserById = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => {
-      if (err.name === "CastError") {
-        res.status(ErrorNotFoundCode).send({ "message" : "Пользователь по указанному _id не найден" });
-      } else {
-        res.status(DefaultErrorCode).send({ "message" : "Ошибка по умолчанию" });
-      }
-    });
-};
+    .orFail(() => {
+      res.status(ErrorNotFoundCode).send({"message": "Пользователь по указанному _id не найден"});
+    })
+    .then((user) => {
+      console.log(user);
+      res.send({data: user})
+    }
+)
+.catch((err) => {
+  console.log("error", err);
+  if (err.name === "CastError") {
+    res.status(ValidationErrorCode).send({ "message" : "Пользователь по указанному _id не найден" });
+  } else {
+    res.status(DefaultErrorCode).send({ "message" : "Ошибка по умолчанию" });
+  }
+});
+}
+;
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  User.create({ name, about, avatar }).then((user) => res.send({ data: user }))
+  const {name, about, avatar} = req.body;
+  User.create({name, about, avatar}).then((user) => res.send({data: user}))
     .catch((err) => {
       if (err.name === "ValidationError") {
-        res.status(ValidationErrorCode).send({ message: "Переданы некорректные данные при создании пользователя" });
+        res.status(ValidationErrorCode).send({message: "Переданы некорректные данные при создании пользователя"});
       } else {
-        res.status(DefaultErrorCode).send({ "message" : "Ошибка по умолчанию" });
+        res.status(DefaultErrorCode).send({"message": "Ошибка по умолчанию"});
       }
     });
 };
 
 module.exports.updateProfile = (req, res) => {
-  const { name } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name }).then((user) => res.send({ data: user }))
+  const {name} = req.body;
+  User.findByIdAndUpdate(req.user._id, {name}).then((user) => res.status(200).send({data: user}))
     .catch((err) => {
       if (err.name === "ValidationError") {
-        res.status(ValidationErrorCode).send({ "message" : "Переданы некорректные данные при обновлении профиля" });
+        res.status(ValidationErrorCode).send({"message": "Переданы некорректные данные при обновлении профиля"});
       } else if (err.name === "CastError") {
-        res.status(ErrorNotFoundCode).send({ "message" : "Пользователь по указанному _id не найден" });
+        res.status(ErrorNotFoundCode).send({"message": "Пользователь по указанному _id не найден"});
       } else {
-        res.status(DefaultErrorCode).send({ "message" : "Ошибка по умолчанию" });
+        res.status(DefaultErrorCode).send({"message": "Ошибка по умолчанию"});
       }
     });
 };
 
 module.exports.updateAvatar = (req, res) => {
-  const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }).then((user) => res.send({ data: user }))
+  const {avatar} = req.body;
+  User.findByIdAndUpdate(req.user._id, {avatar}).then((user) => res.send({data: user}))
     .catch((err) => {
       if (err.name === "ValidationError") {
-        res.status(ValidationErrorCode).send({ "message" : "Переданы некорректные данные при обновлении аватара" });
+        res.status(ValidationErrorCode).send({"message": "Переданы некорректные данные при обновлении аватара"});
       } else if (err.name === "CastError") {
-        res.status(ErrorNotFoundCode).send({ "message" : "Пользователь по указанному _id не найден" });
+        res.status(ErrorNotFoundCode).send({"message": "Пользователь по указанному _id не найден"});
       } else {
-        res.status(DefaultErrorCode).send({ "message" : "Ошибка по умолчанию" });
+        res.status(DefaultErrorCode).send({"message": "Ошибка по умолчанию"});
       }
     });
 };

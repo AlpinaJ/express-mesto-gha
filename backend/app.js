647d4a9d7ec9d884b2f113951bc400d25e94e8b5
julia953197 <julia953197@yandex.ru>
2022-05-21 22:29:42 +0300

    Sprint 15 added bugfix12

--- backend/app.js
+++ backend/app.js
@@ -68,7 +68,7 @@ app.post("/signin", celebrate({
 
 app.post("/signup", celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required().email(),
+    email: Joi.string().required(),
     password: Joi.string().required(),
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
